ce6ad20cd93d0f8b4fd0cab843aca03d44584b8a
James King <151979414+jamesking020773@users.noreply.github.com>
2024-02-21 10:14:47 +1100

    Update urls.py

--- project1/urls.py
+++ project1/urls.py
@@ -4,6 +4,4 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tasks/', include("tasks.urls")),
-    path('newyear/', include("newyear.urls")),
-    path('hello/', include("hello.urls"))
 ]
